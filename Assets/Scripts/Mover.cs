ef3298cfc4f32e949522e1e5c43e491dd70a794b
Gustavo Kemper Weber <g.kemperweber@gmail.com>
2021-03-30 11:25:50 +0100

    second commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Run Rat, Run!");
+        Debug.Log("Use the arrow keys to move the player and dont hit the walls!");
+        Debug.Log("If you choose, you can use the AWSD keys instead the arrow keys");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
